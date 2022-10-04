c9d3ec6652ea980ff231c40b6b5fdaf2711d0181
SteamMomo <ayushstewart22@gmail.com>
2022-10-04 20:03:49 +0530

    basic navigation drawer added

--- app/src/main/java/com/release/ojass2022/MainActivity.java
+++ app/src/main/java/com/release/ojass2022/MainActivity.java
@@ -1,14 +1,45 @@
 package com.release.ojass2022;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class MainActivity extends AppCompatActivity {
+
+    NavigationView navigationView;
+    DrawerLayout drawerLayout;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initialisations();
+        configureToolbar();
     }
+
+    void initialisations(){
+        navigationView = findViewById(R.id.navigation);
+        drawerLayout = findViewById(R.id.main_drawer_layout);
+        toolbar = findViewById(R.id.toolbar);
+    }
+
+    private void configureToolbar() {
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        navigationView.bringToFront();
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+        //navigationView.setNavigationItemSelectedListener((NavigationView.OnNavigationItemSelectedListener) this);
+    }
+
 }
