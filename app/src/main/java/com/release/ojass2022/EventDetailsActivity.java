d4dd58984e747f5ce95a6da4409b2fa0dea1647f
Rajdeep0112 <rajdeepkumar1122002@gmail.com>
2022-10-11 23:23:48 +0530

    event details UI improved

--- app/src/main/java/com/release/ojass2022/EventDetailsActivity.java
+++ app/src/main/java/com/release/ojass2022/EventDetailsActivity.java
@@ -1,9 +1,14 @@
 package com.release.ojass2022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.material.tabs.TabLayout;
 import com.google.android.material.tabs.TabLayoutMediator;
@@ -24,6 +29,21 @@ public class EventDetailsActivity extends AppCompatActivity {
         ViewPagerAdapterEventDetails adapterEventDetails=new ViewPagerAdapterEventDetails(getSupportFragmentManager(),getLifecycle());
         viewPager2DetailsContainer.setAdapter(adapterEventDetails);
 
+        viewPager2DetailsContainer.setClipToPadding(false);
+        viewPager2DetailsContainer.setClipChildren(false);
+        viewPager2DetailsContainer.setOffscreenPageLimit(3);
+        viewPager2DetailsContainer.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer=new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(40));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r=1-Math.abs(position);
+                page.setScaleY(0.85f + r*0.15f);
+            }
+        });
+        viewPager2DetailsContainer.setPageTransformer(compositePageTransformer);
         TabLayoutMediator tabLayoutMediator=new TabLayoutMediator(tabLayoutDetailsOption,viewPager2DetailsContainer, (tab, position) -> {
             tab.setText(adapterEventDetails.getHeader(position));
             tab.setIcon(getResources().getDrawable(iconList[position]));
